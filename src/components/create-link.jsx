211b7e94cded1c6674381f2b94c72a2338ee7ee4
rhrits <hritik.hritiksingh@gmail.com>
2024-07-16 01:14:46 +0530

    domain change

--- src/components/create-link.jsx
+++ src/components/create-link.jsx
@@ -118,7 +118,7 @@ export function CreateLink() {
         />
         {errors.longUrl && <Error message={errors.longUrl} />}
         <div className="flex items-center gap-2">
-          <Card className="p-2">x4x.in</Card> /
+          <Card className="p-2">aayein.xyz</Card> /
           <Input
             id="customUrl"
             placeholder="Custom Link (optional)"
